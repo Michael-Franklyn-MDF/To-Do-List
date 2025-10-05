94b66f22155b290745330e5033a8e4205c94d471
Michael-Franklyn-MDF <michaeldavidfranklyn@gmail.com>
2025-10-06 00:50:16 +0300

    chore: Introduction to To-Do List logic

--- todo.js
+++ todo.js
@@ -1,1 +1,32 @@
-h
+const taskInput = document.getElementById('tasknput')
+const addBtn = document.getElementById('addBtn')
+const taskList = document.getElementById('taskList')
+
+// add new task
+addBtn.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if(taskText === '');
+            
+    const li = document.createElement('Li')
+    li.textContent = taskText;
+
+    //mark as done
+    li.addEventListener('click', () =>{
+        li.classList.toggle('done');
+     })
+    //remove btn
+    const removeBtn = document.createElement('button')
+
+    removeBtn.textContent = 'Remove';
+    removeBtn.classList.add = ('remove-btn');
+    removeBtn.addEventListener('click', () => {
+        e.stopPropagation();
+        li.remove();
+    });
+
+    li.appendChild(removeBtn);
+    taskList.appendChild(li);
+
+    taskInput.value = '';
+
+});
